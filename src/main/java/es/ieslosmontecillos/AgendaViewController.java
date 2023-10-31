76b2d9bcc393f97431d61ad00653e22bdc9198dc
Adrian Gonzalez Garcia <adriangonzalezgarcia54@gmail.com>
2023-10-31 12:55:44 +0100

    segundo

--- src/main/java/es/ieslosmontecillos/AgendaViewController.java
+++ src/main/java/es/ieslosmontecillos/AgendaViewController.java
@@ -3,10 +3,10 @@ package es.ieslosmontecillos;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
@@ -26,6 +26,18 @@ public  class AgendaViewController implements Initializable {
     private TableColumn<Persona,String> columnEmail;
     @FXML
     private TableColumn<Persona,String> columnProvincia;
+    @FXML
+    private Label Nombre;
+    @FXML
+    private Label Apellidos;
+    @FXML
+    private TextField textFieldNombre;
+    @FXML
+    private TextField textFieldApellidos;
+    @FXML
+    private Button Guardar;
+
+    private Persona personaSeleccionada;
 
 
     public void setDataUtil(DataUtil dataUtil){
@@ -57,5 +69,38 @@ public  class AgendaViewController implements Initializable {
                     }
                     return property;
                 });
+        tableViewAgenda.getSelectionModel().selectedItemProperty().addListener(
+                (observable,oldValue,newValue)->{
+                    personaSeleccionada=newValue;
+                    if (personaSeleccionada != null){
+                        textFieldNombre.setText(personaSeleccionada.getNombre());
+                        textFieldApellidos.setText(personaSeleccionada.getApellidos());
+                    } else {
+                        textFieldNombre.setText("");
+                        textFieldApellidos.setText("");
+                    }
+                });
+    }
+
+    @FXML
+    public void onActionButtonGuardar(ActionEvent actionEvent) {
+
+        if (personaSeleccionada != null)
+        {
+            personaSeleccionada.setNombre(textFieldNombre.getText());
+            personaSeleccionada.setApellidos(textFieldApellidos.getText());
+
+            dataUtil.actualizarPersona(personaSeleccionada);
+
+            int numFilaSeleccionada =
+                    tableViewAgenda.getSelectionModel().getSelectedIndex();
+            tableViewAgenda.getItems().set(numFilaSeleccionada,personaSeleccionada);
+
+            TablePosition pos = new
+                    TablePosition(tableViewAgenda,numFilaSeleccionada,null);
+            tableViewAgenda.getFocusModel().focus(pos);
+            tableViewAgenda.requestFocus();
+
+        }
     }
 }
---
+++ src/main/java/es/ieslosmontecillos/PersonaDetalleViewController.java
@@ -0,0 +1,4 @@
+package es.ieslosmontecillos;
+
+public class PersonaDetalleViewController {
+}
